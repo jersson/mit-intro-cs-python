ccdf2510f29a08ea16baf8a03aa03dd9179132a9
Jersson Dongo <Jersson@gmail.com>
2020-02-13 13:59:43 -0500

    Add solution problem03

---
+++ week-02/06-problem-set-02/problem-03/problem.py
@@ -0,0 +1,32 @@
+balance = 999999
+annualInterestRate = 0.18
+#code to be evaluated begin here
+months = 12
+monthlyPaymentRate = annualInterestRate / months
+minimumPayment = balance / months
+maximumPayment = (balance / months) * (1 + monthlyPaymentRate) ** months
+
+def getRemainingBalance(balance, annualInterestRate, minimumPayment):
+    month = 1
+    remainingBalance = balance
+    while month <= 12:
+        unpaidBalance = remainingBalance - minimumPayment
+        interest = annualInterestRate * unpaidBalance / 12.0
+        remainingBalance = unpaidBalance + interest
+        month += 1
+    remainingBalance = round(remainingBalance, 2)
+    return remainingBalance
+
+monthlyPaymentRate = (minimumPayment + maximumPayment) / 2
+remainingBalance = getRemainingBalance(balance, annualInterestRate, monthlyPaymentRate) 
+
+while remainingBalance != 0:
+    if remainingBalance <= 0:
+        maximumPayment = monthlyPaymentRate
+    else:
+        minimumPayment = monthlyPaymentRate
+
+    monthlyPaymentRate = (minimumPayment + maximumPayment) / 2
+    remainingBalance = getRemainingBalance(balance, annualInterestRate, monthlyPaymentRate) 
+print("Lowest Payment: " + str(round(monthlyPaymentRate,2)))
+    
